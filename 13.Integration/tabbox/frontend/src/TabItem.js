cf0bd886013bb961222010e1061a451914f329b6
seojihyeon99 <seojihyeon99@naver.com>
2025-02-11 19:24:08 +0900

    250211 TabBox 과제 - SCSS로 변경 및 CSS 모듈 사용

--- 13.Integration/tabbox/frontend/src/TabItem.js
+++ 13.Integration/tabbox/frontend/src/TabItem.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Tab_Item, Active} from './assets/scss/TabItem.scss';
+import {Tab_Item} from './assets/scss/TabItem.scss';
 
 function TabItem({name, active}) {
     return (
-        <li className={`${Tab_Item} ${active ? Active : ""}`}>
+        <li className={`${Tab_Item} ${active ? "active" : ""}`}>
             {name}
         </li>
     );
