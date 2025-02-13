473ed3daf519420df6407950bf9b3e324e9c4c7a
seojihyeon99 <seojihyeon99@naver.com>
2025-02-13 16:46:17 +0900

    250213 KanbanBoard 과제 - Card 토글 열림/접힘 처리

--- 13.Integration/kanbanboard/frontend/src/Card.js
+++ 13.Integration/kanbanboard/frontend/src/Card.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {_Card, Card_Title, Card_Title_Open} from './assets/scss/Card.scss';
 import TaskList from './TaskList';
 
 function Card({no, title, description, tasks}) {
+    const [isOpen, setIsOpen] = useState(true);
+
+    const openCard = () => {
+        setIsOpen(prev => !prev);
+    }
+
     return (
         <div className={_Card}>
-            <div className={`${Card_Title} ${Card_Title_Open}`}>{title}</div>
-            <div>
-                {description}
-                
-                {/* TaskList */}
-                <TaskList data={tasks}/>
-            </div>            
+            <div className={`${Card_Title} ${isOpen ? Card_Title_Open : ""}`} 
+                onClick={openCard}>
+                {title}
+            </div>
+            {isOpen && (
+                <div>
+                    {description}
+                    
+                    {/* TaskList */}
+                    <TaskList data={tasks}/>
+                </div>     
+            )}
         </div>
     );
 }
